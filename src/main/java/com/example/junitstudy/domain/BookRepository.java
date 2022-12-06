220743a34ce68e12ce7a2deb7cebad57b8493aaf
임빈영 <qlsdud960604@gmail.com>
2022-12-06 10:58:34 +0900

    Modify BookRepository 인터페이스
    애너테이션 설정 추가

--- src/main/java/com/example/junitstudy/domain/BookRepository.java
+++ src/main/java/com/example/junitstudy/domain/BookRepository.java
@@ -1,4 +1,8 @@
 package com.example.junitstudy.domain;
 
-public interface BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
