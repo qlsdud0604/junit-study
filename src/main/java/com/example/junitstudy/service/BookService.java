f3c3390a6c5d61fe32982e67fd7aa1df091735d2
임빈영 <qlsdud960604@gmail.com>
2022-12-06 16:10:03 +0900

    Modify BookService 클래스
    책 등록 서비스 로직 작성

--- src/main/java/com/example/junitstudy/service/BookService.java
+++ src/main/java/com/example/junitstudy/service/BookService.java
@@ -1,7 +1,23 @@
 package com.example.junitstudy.service;
 
+import com.example.junitstudy.domain.Book;
+import com.example.junitstudy.domain.BookRepository;
+import com.example.junitstudy.dto.BookRespDto;
+import com.example.junitstudy.dto.BookSaveReqDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
 @Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Transactional(rollbackOn = RuntimeException.class)
+    public BookRespDto saveBook(BookSaveReqDto dto) {
+        Book bookPS = bookRepository.save(dto.toEntity());
+        return new BookRespDto().toDto(bookPS);
+    }
 }
