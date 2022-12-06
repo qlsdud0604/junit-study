c583ea4377ff30a5ef6612beaa75d34001a3059a
임빈영 <qlsdud960604@gmail.com>
2022-12-06 14:13:07 +0900

    Add BookSaveReqDto 클래스
    사용자로부터 Book 정보를 받는 BookSaveReqDto클래스 생성

---
+++ src/main/java/com/example/junitstudy/dto/BookSaveReqDto.java
@@ -0,0 +1,18 @@
+package com.example.junitstudy.dto;
+
+import com.example.junitstudy.domain.Book;
+import lombok.Setter;
+
+@Setter
+public class BookSaveReqDto {
+    private String title;
+    private String author;
+
+    public Book toEntity() {
+        return Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+    }
+
+}
