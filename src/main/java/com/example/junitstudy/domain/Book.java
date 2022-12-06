c523344a699d88c994d7c0ed7d7669231abcf0ba
임빈영 <qlsdud960604@gmail.com>
2022-12-06 10:43:57 +0900

    Modify Book 클래스
    Book 테이블에 해당하는 Book 클래스의 속성 및 제약조건 정의

--- src/main/java/com/example/junitstudy/domain/Book.java
+++ src/main/java/com/example/junitstudy/domain/Book.java
@@ -1,4 +1,30 @@
 package com.example.junitstudy.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Entity
+@Getter
 public class Book {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 20, nullable = false)
+    private String author;
+
+    @Builder
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
 }
