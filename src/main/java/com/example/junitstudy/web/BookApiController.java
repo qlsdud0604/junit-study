4426df3cde655dd831ca7dcd2de1b09c6ca2af91
임빈영 <qlsdud960604@gmail.com>
2022-12-07 14:28:44 +0900

    Modify BookApiController 클래스
    책 목록 조회 요청 처리 로직 추가

--- src/main/java/com/example/junitstudy/web/BookApiController.java
+++ src/main/java/com/example/junitstudy/web/BookApiController.java
@@ -1,5 +1,7 @@
 package com.example.junitstudy.web;
 
+import com.example.junitstudy.domain.Book;
+import com.example.junitstudy.dto.response.BookListRespDto;
 import com.example.junitstudy.dto.response.BookRespDto;
 import com.example.junitstudy.dto.request.BookSaveReqDto;
 import com.example.junitstudy.dto.response.CMRepsDto;
@@ -10,12 +12,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -39,15 +43,24 @@ public class BookApiController {
         return new ResponseEntity<>(
                 CMRepsDto.builder()
                         .code(1)
-                        .msg("글 저장 성공")
+                        .msg("책 저장 성공")
                         .body(bookRespDto)
-                        .build(), 
+                        .build(),
                 HttpStatus.CREATED
         );
     }
 
+    @GetMapping("/api/v1/book")
     public ResponseEntity<?> getBookList() {
-        return null;
+        BookListRespDto bookListRespDto = bookService.selectBookList();
+        return new ResponseEntity<>(
+                CMRepsDto.builder()
+                        .code(1)
+                        .msg("책 목록 조회 성공")
+                        .body(bookListRespDto)
+                        .build(),
+                HttpStatus.OK
+        );
     }
 
     public ResponseEntity<?> getBook() {
