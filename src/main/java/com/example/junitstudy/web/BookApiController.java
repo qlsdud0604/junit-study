32af869e0d07e41c0031ec53fea624ac7bcb7290
임빈영 <qlsdud960604@gmail.com>
2022-12-07 13:54:11 +0900

    Refactoring BookApiController 클래스
    Validation 적용

--- src/main/java/com/example/junitstudy/web/BookApiController.java
+++ src/main/java/com/example/junitstudy/web/BookApiController.java
@@ -5,11 +5,18 @@ import com.example.junitstudy.dto.request.BookSaveReqDto;
 import com.example.junitstudy.dto.response.CMRepsDto;
 import com.example.junitstudy.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,14 +25,25 @@ public class BookApiController {
     private final BookService bookService;
 
     @PostMapping("/api/v1/book")
-    public ResponseEntity<?> saveBook(@RequestBody BookSaveReqDto reqDto) {
+    public ResponseEntity<?> saveBook(@RequestBody @Valid BookSaveReqDto reqDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError fe : bindingResult.getFieldErrors()) {
+                errorMap.put(fe.getField(), fe.getDefaultMessage());
+            }
+            throw new RuntimeException(errorMap.toString());
+        }
+
         BookRespDto bookRespDto = bookService.saveBook(reqDto);
-        CMRepsDto<?> cmRepsDto = CMRepsDto.builder()
-                .code(1)
-                .msg("Book 저장 성공")
-                .body(bookRespDto)
-                .build();
-        return new ResponseEntity<>(cmRepsDto, HttpStatus.CREATED);   // 201 - insert
+
+        return new ResponseEntity<>(
+                CMRepsDto.builder()
+                        .code(1)
+                        .msg("글 저장 성공")
+                        .body(bookRespDto)
+                        .build(), 
+                HttpStatus.CREATED
+        );
     }
 
     public ResponseEntity<?> getBookList() {
