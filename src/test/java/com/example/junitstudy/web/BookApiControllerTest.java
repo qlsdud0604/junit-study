837ed6cbbe75e04e19e747252779f3be4833b6dd
임빈영 <qlsdud960604@gmail.com>
2022-12-07 16:54:32 +0900

    Test BookApiControllerTest 클래스
    책 목록 조회 기능 통합 테스트 수행

--- src/test/java/com/example/junitstudy/web/BookApiControllerTest.java
+++ src/test/java/com/example/junitstudy/web/BookApiControllerTest.java
@@ -1,13 +1,14 @@
 package com.example.junitstudy.web;
 
+import com.example.junitstudy.domain.Book;
+import com.example.junitstudy.domain.BookRepository;
 import com.example.junitstudy.dto.request.BookSaveReqDto;
 import com.example.junitstudy.service.BookService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +21,16 @@ import static org.assertj.core.api.Assertions.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)   // 랜덤 포트를 이용한 통합 테스트
 public class BookApiControllerTest {
 
+    private static final Integer successCode = 1;
+    private static final String successMsg = "책 목록 조회 성공";
+    private static final String expectedBookTitle = "테스트 책 제목";
+    private static final String expectedBookAuthor = "테스트 책 작가";
+
     @Autowired
     private BookService bookService;
+
+    @Autowired
+    private BookRepository bookRepository;
 
     @Autowired
     TestRestTemplate rt;
@@ -34,6 +43,17 @@ public class BookApiControllerTest {
         om = new ObjectMapper();
         headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @BeforeEach
+    public void saveBookBeforeTest() {
+        String title = expectedBookTitle;
+        String author = expectedBookAuthor;
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
     }
 
     @DisplayName("책 저장 기능 통합 테스트")
@@ -69,10 +89,26 @@ public class BookApiControllerTest {
     public void selectBookList() {
         // given
 
-
         // when
+        HttpEntity<String> request = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = rt.exchange(
+                "/api/v1/book",
+                HttpMethod.GET,
+                request,
+                String.class
+        );
 
         // then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        Integer code = dc.read("$.code");
+        String msg = dc.read("$.msg");
+        String title = dc.read("$.body.items[0].title");
+        String author = dc.read("$.body.items[0].author");
+
+        assertThat(code).isEqualTo(successCode);
+        assertThat(msg).isEqualTo(successMsg);
+        assertThat(title).isEqualTo(expectedBookTitle);
+        assertThat(author).isEqualTo(expectedBookAuthor);
     }
 
 }
